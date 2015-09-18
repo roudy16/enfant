7753a2ab79c15eb19fd8bd58aed81e5c6abb7eb2
Steve Roudebush <roudy@umich.edu>
2015-09-17 22:39:01 -0400

    Update Utility.c
    external linkage for g_string_memory

--- 381_project1/Utility.c
+++ 381_project1/Utility.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int g_string_memory = 0;
+int g_string_memory = 0;
 
 void free_string(char* string_ptr){
     g_string_memory -= (int)strlen(string_ptr);
