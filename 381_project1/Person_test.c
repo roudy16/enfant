8eed0a702a10891446298a35761c425b7f8fffa4
Steve Roudebush <roudy@umich.edu>
2015-09-18 21:59:26 -0400

    tweat

--- 381_project1/Person_test.c
+++ 381_project1/Person_test.c
@@ -4,12 +4,12 @@
 
 int main(void){
     FILE* file_1 = fopen("test_person.out", "w");
-    struct Person* me = create_Person("Steve", "Roudy", "312");
+    struct Person* me = create_Person("SteveSteveSteveSteveSteveSteveSteveSteveSteveSteveSteveSteve12345678", "Roudy", "312");
     print_Person(me);
     printf("My lastname is %s\n", get_Person_lastname(me));
     printf("String bytes allocated: %d\n", g_string_memory);
     save_Person(me, file_1);
-    //fclose(file_1);
+
     FILE* file_2 = fopen("test_person.out", "r");
     struct Person* me_too = load_Person(file_2);
     printf("String bytes allocated: %d\n", g_string_memory);
